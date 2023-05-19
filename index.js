182924707a725987653c5f45c992926f4b04b777
Michael Perkins <mperkins1995@gmail.com>
2023-05-18 19:27:47 -0500

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Michael Perkins is the champion";
+document.body.append(newHeader);
